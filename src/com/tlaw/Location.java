d03d1d445729f3a21451f49f7f03ccaea898606e
Tim Lawless <timothy.j.lawless@outlook.com>
2023-01-26 14:40:05 -0500

    running

--- src/com/tlaw/Location.java
+++ src/com/tlaw/Location.java
@@ -14,9 +14,7 @@ public class Location {
             this.exits = new HashMap<String, Integer>(exits);
             this.exits.put("Q", 0);
       }
-//public void addExit(String direction, int location){
-//            exits.put(direction, location);
-//}
+
       public int getLocationID() {
             return locationID;
       }
